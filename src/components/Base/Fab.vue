610e1b18c12bb3d9fb0547775b913260c721c754
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-06-22 23:17:18 +0700

    add base component

---
+++ src/components/Base/Fab.vue
@@ -0,0 +1,66 @@
+<script
+  setup
+  lang="ts"
+>
+interface PropsInterface {
+  /**
+   * Applies position: absolute to the component.
+   */
+  absolute?: boolean
+  /**
+   * Applies position: fixed to the component.
+   */
+  fixed?: boolean
+  /**
+   * Aligns the content towards the top.
+   */
+  top?: boolean
+  /**
+   * Aligns the content towards the bottom.
+   */
+  bottom?: boolean
+  /**
+   * Aligns the content towards the left.
+   */
+  left?: boolean
+  /**
+   * Aligns the content towards the right.
+   */
+  right?: boolean
+}
+
+const props = defineProps<PropsInterface>()
+
+/**
+ * Base class
+ */
+const baseClass = [
+  'w-[56px]',
+  'h-[56px]',
+  'flex',
+  'items-center',
+  'justify-center',
+  'shadow-[0px_4px_8px_3px_rgba(0,0,0,0.15]',
+  'bg-[color:var(--primary)]',
+  'text-[color:var(--on-primary)]',
+  'p-[16px]',
+  'rounded-2xl',
+  'drop-shadow-[0px_1px_3px_rgba(0,0,0,0.3)]',
+]
+
+const classes = computed(() => [
+  ...baseClass,
+  ...[props.absolute && 'absolute'],
+  ...[props.fixed && 'fixed'],
+  ...[props.top && 'top-4'],
+  ...[props.bottom && 'bottom-4'],
+  ...[props.left && 'left-4'],
+  ...[props.right && 'right-4'],
+])
+</script>
+
+<template>
+  <button :class="classes">
+    <slot />
+  </button>
+</template>
--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,22 +1,38 @@
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
 const props = defineProps<{
   href?: string
   to?: string
   target?: string
 }>()
 
-const cssClasses = 'px-5 py-3 bg-blue-100 rounded-full capitalize text-blue-600 flex items-center justify-center gap-x-2 transition duration-500 hover:bg-blue-200 w-full'
+const cssClasses =
+  'px-5 py-3 bg-blue-100 rounded-full capitalize text-blue-600 flex items-center justify-center gap-x-2 transition duration-500 hover:bg-blue-200 w-full'
 </script>
 
 <template>
   <div class="inline-block">
-    <a v-if="props.href" :href="props.href" :class="cssClasses" :target="target">
+    <a
+      v-if="props.href"
+      :href="props.href"
+      :class="cssClasses"
+      :target="target"
+    >
       <slot />
     </a>
-    <router-link v-else-if="props.to" :to="props.to" :class="cssClasses">
+    <router-link
+      v-else-if="props.to"
+      :to="props.to"
+      :class="cssClasses"
+    >
       <slot />
     </router-link>
-    <button v-else :class="cssClasses">
+    <button
+      v-else
+      :class="cssClasses"
+    >
       <slot />
     </button>
   </div>
