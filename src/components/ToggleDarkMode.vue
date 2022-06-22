0b1e376890fa41b3805af911355018e31ddd03ea
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-06-22 23:20:00 +0700

    change how toggle dark mode works

--- src/components/ToggleDarkMode.vue
+++ src/components/ToggleDarkMode.vue
@@ -1,19 +1,33 @@
-<script setup lang="ts">
+<script
+  setup
+  lang="ts"
+>
 import { isDark, toggleDark } from '~/composables/dark'
 
-const iconClass = 'text-blue-600 h-7 w-7'
+const classes = ref(['w-[24px]', 'h-[24px]'])
 </script>
 
 <template>
-  <button class="fixed p-3 bg-white rounded-full shadow-2xl bottom-7 right-7" @click="toggleDark()">
+  <Fab
+    fixed
+    bottom
+    right
+    @click="toggleDark()"
+  >
     <Transition
       enter-from-class="scale-50 opacity-0"
-      enter-active-class="duration-300"
+      enter-active-class="duration-200"
       enter-to-class="scale-100 opacity-1"
       mode="out-in"
     >
-      <IMdiBrightness7 v-if="isDark" :class="iconClass" />
-      <IMdiBrightness2 v-else :class="iconClass" />
+      <IMdiBrightness7
+        v-if="isDark"
+        :class="classes"
+      />
+      <IMdiBrightness2
+        v-else
+        :class="classes"
+      />
     </Transition>
-  </button>
+  </Fab>
 </template>
