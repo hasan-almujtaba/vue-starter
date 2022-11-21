91fcb0355f728eb28bb98a82dd0edc108c30af13
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-11-21 17:39:15 +0700

    refactor: update default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,25 +1,7 @@
 <template>
   <div>
-    <!-- Navigation -->
-    <AppBar>
-      <AppBarTitle> Vue Starter </AppBarTitle>
-      <Spacer />
-      <Button
-        href="https://github.com/hasan-almujtaba/vue-starter"
-        target="_blank"
-      >
-        <IMdiGithub class="w-[18px] h-[18px]" />
-        View on Github
-      </Button>
-    </AppBar>
-    <!-- Navigation -->
-
     <!-- Page Content -->
     <RouterView />
     <!-- Page Content -->
-
-    <!-- Dark Mode Button -->
-    <ToggleDarkMode />
-    <!-- Dark Mode Button -->
   </div>
 </template>
